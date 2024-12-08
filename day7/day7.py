29345c4bd0d4b234726cab34191bb6487b499032
B33Boy <abhip202@gmail.com>
2024-12-08 14:26:53 -0500

    day7 part 1

--- day7/day7.py
+++ day7/day7.py
@@ -1,3 +1,5 @@
+from itertools import product, zip_longest
+
 
 def equals_target(vals, target):
     def dfs(i, prodsum):
@@ -12,6 +14,38 @@ def equals_target(vals, target):
     return dfs(0, vals[0])
 
 
+def equals_target2(vals, target):
+    # Fails because eval follows BEDMAS and here we want left-to-right
+
+    n = len(vals)
+
+    # n-1 gaps in between n values
+    operations = product(["+", "*"], repeat=n-1)
+    for ops in operations:
+        expression = ''.join((a or '') + (b or '')
+                             for a, b in zip_longest(vals, ops,  fillvalue=''))
+        print(expression)
+        if eval(expression) == target:
+            return True
+
+    return False
+
+
+def equals_target3(vals, target):
+    n = len(vals)
+
+    # n-1 gaps in between n values
+    operations = product(["+", "*"], repeat=n-1)
+
+    for ops in operations:
+        expression = vals[0]
+        for val, operator in zip(vals[1:], ops):
+            expression = f"({expression}{operator}{val})"
+        if eval(expression) == target:
+            return True
+    return False
+
+
 res = 0
 
 with open('inp.txt', 'r') as f:
@@ -21,8 +55,9 @@ with open('inp.txt', 'r') as f:
         tot = int(tot)
 
         vals = list(map(int, vals.split()))
+        # vals = vals.split()  # For equals_target2
 
-        if equals_target(vals, tot):
+        if equals_target3(vals, tot):
             res += tot
 
 print(res)
