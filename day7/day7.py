ef528314105f0561bca94b39fc68b0e0580aa845
B33Boy <abhip202@gmail.com>
2024-12-09 01:26:47 -0500

    day7 part 2 working

--- day7/day7.py
+++ day7/day7.py
@@ -1,3 +1,4 @@
+import re
 from itertools import product, zip_longest
 
 
@@ -58,6 +59,94 @@ with open('inp.txt', 'r') as f:
         # vals = vals.split()  # For equals_target2
 
         if equals_target3(vals, tot):
+            res += tot
+
+# print(res)
+
+
+# Part 2
+res = 0
+
+
+def eval_expression(expr):
+    if "||" not in expr:
+        return eval(expr)
+
+    expr = expr.split("||")
+    print(expr)
+    tmp = ""
+    for count, elem in enumerate(expr):
+        if count == 0:
+            continue
+
+
+def equals_target4(vals, target):
+    n = len(vals)
+
+    # n-1 gaps in between n values
+    operations = product(["+", "*", "||"], repeat=n-1)
+
+    # pattern = r"(\d)\)(\d)"
+    pattern = r"\)(?=\d|\))"
+
+    for ops in operations:
+        expression = vals[0]
+
+        for val, operator in zip(vals[1:], ops):
+            expression = f"({expression}{operator}{val})"
+
+            if operator == '||':
+                print("original: ", expression)
+                expression = expression.replace("||", "")
+
+                print("before: ", expression)
+                num_subs = len(re.findall(pattern, expression))
+                expression = re.sub(
+                    pattern, r"", expression) + num_subs*')'
+
+            print("after: ", expression, "\n")
+
+            if eval(expression) == target:
+                return True
+
+    return False
+
+
+def equals_target5(vals, target):
+    n = len(vals)
+
+    # n-1 gaps in between n values
+    operations = product(["+", "*", "||"], repeat=n-1)
+    for ops in operations:
+        expression = vals[0]
+        i = 1
+
+        for operator in ops:
+            if operator == "+":
+                expression += vals[i]
+            elif operator == '*':
+                expression *= vals[i]
+            elif operator == '||':
+                expression = int(str(expression) + str(vals[i]))
+
+            i += 1
+
+        if expression == target:
+            return True
+
+    return False
+
+
+with open('inp.txt', 'r') as f:
+    for line in f:
+
+        tot, vals = line.rstrip().split(':')
+        tot = int(tot)
+
+        vals = list(map(int, vals.split()))
+        # vals = vals.split()  # For equals_target2
+
+        if equals_target5(vals, tot):
             res += tot
 
 print(res)
